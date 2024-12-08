27a68c312d0e5cc688589c22cd7259be0a7ba8a2
Shpetim <shpetim.alimi@ndbit.net>
2024-12-08 12:57:43 +0100

    set normal face function

--- src/hittable.rs
+++ src/hittable.rs
@@ -7,6 +7,18 @@ pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f32,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        let front_face = r.direction().dot(&outward_normal) < 0.0;
+        if front_face {
+            self.normal = -*outward_normal
+        } else {
+            self.normal = *outward_normal;
+        }
+    }
 }
 
 pub trait Hittable {
